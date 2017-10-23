bbd100ec5e67201bf417449d5192e68e71f74d94
hannahmbee <31848924+hannahmbee@users.noreply.github.com>
2017-10-23 18:02:26 +0100

    adding event.preventDefault()

--- js/script.js
+++ js/script.js
@@ -2,13 +2,12 @@ $(document).ready(function() {
 
 	//Prevent form submission w/ event.preventDefault()//
 
-//Really stuck on how to do this? There doesn't seem to be a form to .preventDefault() on? Or do I need to make one?//
-
 	/*User clicks "Read More" on primary column
 		Then show <p> tag and "Read Less" by sliding down
 		Hide "Read More"*/
 
-	$('.readmore').click(function() {
+	$('.readmore').click(function(event) {
+		event.preventDefault();
 		$('#show-this-on-click').slideDown();
 		$('.readmore').hide();
 		$('.readless').show();
@@ -18,7 +17,8 @@ $(document).ready(function() {
 		Then hide <p> and "Read Less" by sliding up
 		Show "Read More"*/
 
-	$('.readless').click(function() {
+	$('.readless').click(function(event) {
+		event.preventDefault();
 		$('#show-this-on-click').slideUp();
 		$('.readmore').show();
 		$('.readless').hide();
@@ -28,7 +28,8 @@ $(document).ready(function() {
 		Then show <p> tag and "Read Less" by sliding down
 		Hide "Learn More"*/
 
-	$('.learnmore').click(function() {
+	$('.learnmore').click(function(event) {
+		event.preventDefault()
 		$('#learnmoretext').slideDown();
 		$('.learnmore').hide();
 	})
